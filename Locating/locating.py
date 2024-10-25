1b171bd61b5d4679466a4edb67b68a1c220dd756
Lucky-voyage <113913835+Lucky-voyage@users.noreply.github.com>
2024-10-25 08:40:38 +0800

    locating algorithm updating

--- Locating/locating.py
+++ Locating/locating.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 import torch.nn.functional as F
 from transformers import AutoModelForCausalLM, AutoTokenizer, GPT2LMHeadModel, GPT2TokenizerFast
@@ -6,12 +7,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, GPT2LMHeadModel, G
 class Config:
     checkpoint = "/home/wanyao/wcloong/Locating-and-Updating-Code-Knowledge-for-Code-LM/hf-model/codeparrot"
     dif_step = 20 # differantial step, we transform the continuous differantial computing into discrete steps
+    top_k = 20 # sample the top-k neurons as pivot neurons
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def single_step_for_neuron_score(
-        X: str,
-        Y: str,
+        inputs: torch.tensor,
         position: tuple,    # (layer, dim, index)
         model: GPT2LMHeadModel,
         tok: GPT2TokenizerFast,
@@ -21,67 +22,104 @@ def single_step_for_neuron_score(
     We divide the computing into m steps. Each step holds different paras for target token.
     In this method, we will calculate the score of one step, where the para of the neuron is froze.
     
-    @para: X, the pre-text prompt
-    @para: Y, the target output
-    @para: position, a tuple with 3 elements, (layer, dim, index), to decide the position of one neuron
-    @para: model, huggingface model, GPT2LMHeadModel
-    @para: tok, huggingface tokenizer, GPT2TokenizerFast 
+    Args:
+        X(str): the pre-text prompt
+        Y(str): the target output
+        position(tuple): a tuple with 3 elements, (layer, dim, index), to decide the position of one neuron
+        model(GPT2LMHeadModel)
+        tok(GPT2TokenizerFast)
+        config(Config)
+
+    Returns:
+        torch.tensor: the gradient of neuron with current value
     '''
-    model.to(config.device)
-    for para in model.parameters():
-        para.requires_grad = True
-    model.train()
-
-    token_x = tok(X, return_tensors='pt').input_ids.to(config.device)
-    token_y = tok(Y, return_tensors='pt').input_ids.to(config.device)
-    len_y = token_y.size(-1)
-
     target_probs = []   # store the probability in each inference
     target_grad = []    # store the grad of target neuron in each inference
-    for i in range(len_y):
-        input_ids = torch.cat((token_x, token_y[:, :i]), dim=-1)
-        logits = model(input_ids).logits[:, -1, :]
-        probs = F.softmax(logits, dim=-1)
+    
+    logits = model(inputs).logits[:, -1, :]
+    probs = F.softmax(logits, dim=-1)
 
-        target_prob = probs[-1, token_y[0, -1]]
-        target_probs.append(target_prob)
+    target_prob = probs[:, token_y[0, -1]]
+    target_probs.append(target_prob)
 
-        model.zero_grad()
+    model.zero_grad()
+    target_prob.backward()
 
-        target_prob.backward()
-
-        for name, para in model.named_parameters():
-            if name == f'transformer.h.{position[0]}.mlp.c_proj.weight':
-                target_grad.append(para[position[1], position[2]])
-                break
+    for name, para in model.named_parameters():
+        if name == f'transformer.h.{position[0]}.mlp.c_proj.weight':
+            target_grad.append(para[position[1], position[2]])
+            break
 
     part_score = torch.sum(
         torch.tensor(target_grad, device=config.device) / 
         torch.tensor(target_prob, device=config.device))
     return part_score
 
+
 def score_computing_for_single_neuron(
         X: str,
         Y: str,
-        positoin: tuple,
+        position: tuple,
         model: GPT2LMHeadModel,
         tok: GPT2TokenizerFast,
         config: Config
 ):
-    model_copy = GPT2LMHeadModel(model.config)
-    model_copy.load_state_dict(model.state_dict())
+    '''
+    For each neuron, locating algorithm will conduct multiple steps to computing neuron's gradient with different value
 
-    orig_weight = model_copy.transformer.h[position[0]].mlp.c_proj.weight[position[1], position[2]]
+    Args:
+        X(str): the pre-text prompt
+        Y(str): the target output
+        position(tuple): a tuple with 3 elements, (layer, dim, index), to decide the position of one neuron
+        model(GPT2LMHeadModel)
+        tok(GPT2TokenizerFast)
+        config(Config)
 
+    Returns:
+        torch.tensor: the score of one neuron  
+    
+    '''
+    model.to(config.device)
+    for para in model.parameters():
+        para.requires_grad = True
+    model.train()
+
+    model_copy = copy.deepcopy(model)
+    orig_weight = model.transformer.h[position[0]].mlp.c_proj.weight[position[1], position[2]].clone()
+    
+    token_x = tok(X, return_tensors='pt').input_ids.to(config.device)
+    token_y = tok(Y, return_tensors='pt').input_ids.to(config.device)
+    len_y = token_y.size(-1)
+    input_batch = []
+    target_token = []
+    for i in range(len_y - 1):
+        input_ids = torch.cat((token_x, token_y[:, :i]), dim=-1)
+        input_batch.append(input_ids)
+        target_token.append(token_y[:, i])
+    
     score = 0.
+    import time
+    a = time.time()
     for i in range(config.dif_step):
         new_weight = float(i / config.dif_step) * orig_weight
-        model_copy.transformer.h[position[0]].mlp.c_proj.weight[position[1], position[1]] = new_weight
+        with torch.no_grad():
+            model_copy.transformer.h[position[0]].mlp.c_proj.weight[position[1], position[1]] = new_weight
 
         score += single_step_for_neuron_score(X, Y, position, model_copy, tok, config)
-
+    b = time.time()
+    print(f'time: {b - a}')
     return score / (config.dif_step + 1) * orig_weight
 
+
+def locating_pivot_neurons(
+    X: str,
+    Y: str,
+    model: GPT2LMHeadModel,
+    tok: GPT2TokenizerFast,
+    config: Config
+):
+
+    return
 
 
 
@@ -101,4 +139,4 @@ tok = AutoTokenizer.from_pretrained(config.checkpoint)
 
 # single_step_for_neuron_score(X, Y, position, model, tok, config)
 
-score_computing_for_single_neuron(X, Y, position, model, tok, config)
+print(score_computing_for_single_neuron(X, Y, position, model, tok, config))
